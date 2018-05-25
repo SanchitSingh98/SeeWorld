0ce5a9c5da3a3e9a2f393627b7996fe91dca46c0
Sanchit Singh <sanchitsingh@d-10-19-82-209.dhcp4.washington.edu>
2018-05-25 11:57:32 -0700

    See World app. Assumes a storyboard with a UIImageView and an action button

--- SeeFood-CoreML/ViewController.swift
+++ SeeFood-CoreML/ViewController.swift
@@ -1,25 +1,80 @@
-//
-//  ViewController.swift
-//  SeeFood-CoreML
-//
-//  Created by Angela Yu on 27/06/2017.
-//  Copyright © 2017 Angela Yu. All rights reserved.
-//
-
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var classificationResults : [VNClassificationObservation] = []
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func detect(image: CIImage) {
+        
+        // Load the ML model through its generated class
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("can't load ML model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first
+                else {
+                    fatalError("unexpected result type from VNCoreMLRequest")
+                }
+            
+            
+                self.navigationItem.title = "It is a :" + topResult.identifier
+                self.navigationController?.navigationBar.barTintColor = UIColor.green
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do { try handler.perform([request]) }
+        catch { print(error) }
+        
+        
+        
     }
+    
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+                    
+            imageView.image = image
+            
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+            
+            guard let ciImage = CIImage(image: image) else {
+                fatalError("couldn't convert uiimage to CIImage")
+            }
+            
+            detect(image: ciImage)
+            
+        }
+    }
+        
 
-
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+   
 }
 
